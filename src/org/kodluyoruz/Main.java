b30a62eb2cbb5dc984c0a42da443f92aa2e34349
esmahaber <l1621012063@stud.sdu.edu.tr>
2021-01-05 14:43:19 +0300

    QueueList override methods completed

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -3,7 +3,20 @@ package org.kodluyoruz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        QueueList<Integer> myList =new QueueList<>();
+
+        myList.add(1);
+        myList.add(2);
+
+
+        System.out.println(myList.poll());
+        System.out.println(myList.poll());
+        System.out.println(myList.peek());
+        System.out.println(myList.peek());
+        System.out.println(myList.poll());
+        System.out.println(myList.peek());
+        System.out.println(myList.poll());
+        System.out.println(myList.peek());
 
     }
 }
--- src/org/kodluyoruz/QueueList.java
+++ src/org/kodluyoruz/QueueList.java
@@ -1,19 +1,47 @@
 package org.kodluyoruz;
 
 class QueueList<T>  implements Queue<T> {
+    private Node<T> firstNode;
+    private Node<T> nextNode;
+    private int length;
+
+    public QueueList(){
+        this.length = 0;
+    }
 
     @Override
     public void add(T value) {
-
+        if(length == 0){
+            firstNode = new Node();
+            nextNode = firstNode;
+            length++;
+        }else{
+            nextNode.nextNode= new Node();
+            nextNode = nextNode.nextNode;
+            length++;
+        }
+        nextNode.startNode = value;
+        nextNode.nextNode = null;
     }
 
     @Override
     public T poll() {
-        return null;
+            Node<T> node = firstNode;
+            if (length != 0) {
+                length--;
+                firstNode = firstNode.nextNode;
+                if (firstNode == null) {
+                    nextNode = null;
+                    length = 0;
+                }
+            }else {
+                return null;
+            }
+            return node.startNode;
     }
 
     @Override
     public T peek() {
-        return null;
+        return firstNode != null ?  firstNode.startNode : null;
     }
 }
