a7c0ac98a9c2e7de8298b1e93e8b21d8403a252b
esmahaber <l1621012063@stud.sdu.edu.tr>
2021-01-05 13:49:29 +0300

    QueueList implements Queue

--- src/org/kodluyoruz/QueueList.java
+++ src/org/kodluyoruz/QueueList.java
@@ -1,5 +1,19 @@
 package org.kodluyoruz;
 
-class QueueList<T>  {
+class QueueList<T>  implements Queue<T> {
 
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public T poll() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
 }
